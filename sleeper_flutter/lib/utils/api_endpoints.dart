b8ace5fd85f966b6ad712aa4b0de292168a0e629
Johanes Raphael Nandaputra <johanesraphaeln@gmail.com>
2024-01-11 20:24:39 +0700

    Add API Endpoints

--- sleeper_flutter/lib/utils/api_endpoints.dart
+++ sleeper_flutter/lib/utils/api_endpoints.dart
@@ -1,10 +1,25 @@
 // ignore_for_file: prefer_const_declarations, library_private_types_in_public_api
 
-class ApiEndpoints {
-  static final String baseUrl = 'http://10.0.2.2:9000/'; //localhost
-  static _AuthEndPoints authEndPoints = _AuthEndPoints();
+class APIEndpoints {
+  static final String baseUrl = 'http://10.0.2.2:9000/api/'; //localhost
+  static _APIEndpoints authEndPoints = _APIEndpoints();
 }
 
-class _AuthEndPoints {
+class _APIEndpoints {
   final String register = 'auth/register';
+
+  final String getAllFoods = 'foods';
+  final String getOneFood = 'foods/:id';
+
+  final String getAllExercises = 'exercises';
+  final String getOneExercise = 'exercises/:id';
+
+  final String getAllFeedbacks = 'feedbacks';
+  final String getOneFeedback = 'feedbacks/:id';
+
+  final String inputDailyFeedbacks = 'feedbacks/daily';
+  final String inputWeeklyFeedbacks = 'feedbacks/weekly';
+
+  final String getFoodRecommendations = 'recommendations/food';
+  final String getExerciseReccomendations = 'recommendations/exercise';
 }
